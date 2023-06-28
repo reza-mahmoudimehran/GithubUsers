3b6cb72019605fd780c6bfb27ac6175205614689
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-06-29 02:09:28 +0430

    Setup Navigation Component

--- app/src/main/java/ir/reza_mahmoudi/githubusers/core/presentation/MainActivity.kt
+++ app/src/main/java/ir/reza_mahmoudi/githubusers/core/presentation/MainActivity.kt
@@ -2,8 +2,10 @@ package ir.reza_mahmoudi.githubusers.core.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ir.reza_mahmoudi.githubusers.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/ir/reza_mahmoudi/githubusers/feature_search/presentation/SearchFragment.kt
@@ -0,0 +1,29 @@
+package ir.reza_mahmoudi.githubusers.feature_search.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import ir.reza_mahmoudi.githubusers.R
+import ir.reza_mahmoudi.githubusers.databinding.FragmentSearchBinding
+
+@AndroidEntryPoint
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+//        binding = null
+    }
+}
