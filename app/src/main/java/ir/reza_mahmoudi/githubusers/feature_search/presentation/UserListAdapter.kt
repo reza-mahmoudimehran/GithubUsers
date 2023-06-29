d3df56b335ad03e935880f6a3f5f12c4b38e7817
reza-mahmoudimehran <reza.mahmoudi.mehran@gmail.com>
2023-06-29 14:16:40 +0430

    Add User List Adapter

---
+++ app/src/main/java/ir/reza_mahmoudi/githubusers/feature_search/presentation/UserListAdapter.kt
@@ -0,0 +1,45 @@
+package ir.reza_mahmoudi.githubusers.feature_search.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ir.reza_mahmoudi.githubusers.databinding.ItemUserBinding
+import ir.reza_mahmoudi.githubusers.feature_search.domain.model.User
+
+class UserListAdapter (
+    private val onItemClicked: (detailsUrl: String?) -> Unit
+) : ListAdapter<User, UserListAdapter.ViewHolder>(
+    DiffCallback
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+        holder.itemView.setOnClickListener {
+            onItemClicked(getItem(position).url)
+        }
+    }
+
+    class ViewHolder(private val binding: ItemUserBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(user: User){
+            binding.user=user
+        }
+    }
+
+    companion object {
+        val DiffCallback = object : DiffUtil.ItemCallback<User>(){
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem.login == newItem.login
+            }
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
